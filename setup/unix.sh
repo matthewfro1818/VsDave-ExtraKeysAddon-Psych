c5c9bea1dba0fc183bd6035d40d0516a07beb061
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-11-29 11:38:20 -0500

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -9,12 +9,13 @@ echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
 haxelib install lime 8.1.2
 haxelib install openfl 9.3.3
-haxelib git flixel https://github.com/matthewfro18/flixel-3
+haxelib install flixel 4.11.0
 haxelib install flixel-addons 2.11.0
 haxelib install flixel-tools
 haxelib install flixel-ui 2.6.1
 haxelib install actuate 1.9.0 
-haxelib install hxCodec 2.5.1
+haxelib install hxCodec 2.5.1          
+haxelib install linc_luajit
 haxelib install hscript
 haxelib git linc_luajit https://github.com/nebulazorua/linc_luajit
 haxelib git hscript-ex https://github.com/ianharrigan/hscript-ex
